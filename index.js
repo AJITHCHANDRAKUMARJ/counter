8a463cedfc540458403fcceeef2329cba6500cdb
AJITHCHANDRAKUMARJ <96830282+AJITHCHANDRAKUMARJ@users.noreply.github.com>
2023-10-09 19:46:47 +0530

    v2 Update index.js
    Added "count=0" in save function

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ let count = 0
 let x = document.getElementById("increment-dis")
 let y = document.getElementById("finish")
 
-function incr(){                       //Incrrement button//
+function incr(){                      //Incrrement button//
 
     count = count + 1  
     x.innerHTML = count 
 }
 
-function save(){                     //Save button//
+function save(){                      //Save button//
 
   let Y = count + " -"
-   y.innerHTML += Y   
-}
+   y.innerHTML += Y                   // if you want to start the 
+    count = 0                         // count from zero again then
+}                                     //add the "count = 0" line
 
 
 
